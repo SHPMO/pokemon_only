043791adea849038bfc8b6c37b4d04d7916c97df
sunoru <sunoru@live.com>
2015-05-24 00:47:31 +0800

    working on homepage

--- pmo2015/static/pmo2015/js/home.js
+++ pmo2015/static/pmo2015/js/home.js
@@ -1,11 +1,16 @@
 function autoHeight() {
     var h = $(window).height();
     var w = $(window).width();
+    var ph = Math.floor(h / 2) + 180;
     $('body').height(h);
-    $('#home-header').height(Math.floor(h / 2) - 271);
-    $('.home-footer-side').width(Math.floor((w - 1350) / 2));
-    $('#home-footer').height(Math.floor(h / 2) - 219);
-    $('#home-footer-center-bottom').height(Math.floor(h / 2) - 314);
+//    $('#home-header').height(Math.floor(h / 2) - 271);
+    $('#home-main-container').height(ph);
+    var qh = ph - 506;
+    if (qh<0) qh = 0;
+    $('#home-content').css({"top": qh});
+    $('.home-footer-side').width(Math.round((w - 1350) / 2));
+    $('#home-footer').height(ph - 405);
+    $('#home-footer-center-bottom').height(ph - 500);
 }
 
 $(document).ready(function (){

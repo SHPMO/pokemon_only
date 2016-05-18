85634a97f1f5ab0fbf9dc25650c6c89ced58a47a
sunoru <s@sunoru.com>
2016-05-18 19:49:19 +0800

    Maintaining...

--- msite/views.py
+++ msite/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User, Group, Permission
+from django.http import Http404
 
 
 def home(request):
+    raise Http404
     return render(request, 'home.html')
